d48f9dfe5faaac2ab158912b963d709315bcbe63
kishormote <148129053+kishormote@users.noreply.github.com>
2025-02-12 01:06:27 +0530

    BD-2: Added Get Roles API

---
+++ src/main/java/com/tractorental/fullstack_backend/roles/Role.java
@@ -0,0 +1,23 @@
+package com.tractorental.fullstack_backend.roles;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ROLES")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String roleName;
+}

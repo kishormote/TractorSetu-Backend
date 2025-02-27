439bc0cafdeaf472af61a74222deef40786120ba
kishormote <148129053+kishormote@users.noreply.github.com>
2025-02-27 21:08:49 +0530

    Bug-1 : User id added in JWT token

--- src/main/java/com/tractorental/fullstack_backend/configuration/JwtUtil.java
+++ src/main/java/com/tractorental/fullstack_backend/configuration/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.tractorental.fullstack_backend.configuration;
 
+import com.tractorental.fullstack_backend.auth.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtUtil {
@@ -16,9 +18,10 @@ public class JwtUtil {
     private final String SECRET_KEY = "b7X$eN@k9Lz!qP3&fD4*RmUv#Y2oT6W%";
     private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
 
-    public String generateToken(UserDetails userDetails) {
+    public String generateToken(UserDetails userDetails, User user) {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
+                .addClaims(Map.of("id", user.getId()))
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(key, SignatureAlgorithm.HS256)
